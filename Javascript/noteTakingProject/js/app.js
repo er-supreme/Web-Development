7ebbc36ee89778659c89d82a9a2763755391369a
er-Supreme <er.coder.me@gmail.com>
2022-02-21 13:40:54 +0530

    title is added in note project

--- Javascript/noteTakingProject/js/app.js
+++ Javascript/noteTakingProject/js/app.js
@@ -1,17 +1,24 @@
-showNotes();
+// showNotes();
 
 let addNoteBtn = document.getElementById("addNoteBtn");
 
 addNoteBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes); //to make it to array
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value,
+  };
+  notesObj.push(myObj);
+
   localStorage.setItem("notes", JSON.stringify(notesObj)); //to convert array into strings
+  addTitle.value = "";
   addTxt.value = "";
   //console.log(notesObj);
   showNotes();
@@ -24,13 +31,14 @@ function showNotes() {
   } else {
     notesObj = JSON.parse(notes);
   }
+
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
     <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">Note ${index + 1}</h5>
-                <p class="card-text"> ${element}</p>
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text"> ${element.text}</p>
                 <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
             </div>
         </div>`;
