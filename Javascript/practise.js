8b826540c3ec1b901947d9ed4ec4699776702ea9
er-Supreme <er.coder.me@gmail.com>
2022-02-22 19:56:55 +0530

    college Library Project made

--- Javascript/practise.js
+++ Javascript/practise.js
@@ -1,5 +1,6 @@
 console.log('this is sachin');
-let a = window.innerWidth;
-let b = window.innerHeight;
-window.blur(a);
-console.log(document.documentElement,a.blur);
+// let a = window.innerWidth;
+// let b = window.innerHeight;
+// window.blur(a);
+// console.log(document.documentElement,a.blur);
+
