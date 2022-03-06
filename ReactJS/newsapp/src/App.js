776311bbd0ce00971938f2e6bd41c55003c8831e
er-Supreme <er.coder.me@gmail.com>
2022-03-06 21:13:50 +0530

    inotes full stack project made have backend code how to connect to mongoose

---
+++ ReactJS/newsapp/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+      <NavBar/>
+      <News/>
+      </>
+    )
+  }
+}
+
+export default App
